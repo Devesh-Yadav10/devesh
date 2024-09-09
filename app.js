bf22ff7d50210518e03923a4445c82aeb39dcfe2
Devesh Yadav <160987201+Devesh-Yadav10@users.noreply.github.com>
2024-09-09 17:13:40 +0530

    Initial commit

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 $(".right img").on('mouseover',function(){
     $(".hover-text").animate({opacity:1},300)
 });
@@ -7,3 +8,14 @@ $(".right img").on('mouseout',function(){
 });
 
 
+=======
+$(".right img").on('mouseover',function(){
+    $(".hover-text").animate({opacity:1},300)
+});
+$(".right img").on('mouseout',function(){
+    $(".hover-text").animate({opacity:0},200)
+
+});
+
+
+>>>>>>> 4d74a34f2ca20c3ef844325f8b5c2baeb285486e
