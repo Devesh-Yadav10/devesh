ff72a901c791cc3bb801fd5faf800e9054c2fc4e
Devesh Yadav <160987201+Devesh-Yadav10@users.noreply.github.com>
2024-09-09 08:28:21 +0530

    second try

---
+++ app.js
@@ -0,0 +1,9 @@
+$(".right img").on('mouseover',function(){
+    $(".hover-text").animate({opacity:1},300)
+});
+$(".right img").on('mouseout',function(){
+    $(".hover-text").animate({opacity:0},200)
+
+});
+
+
